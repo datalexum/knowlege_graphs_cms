9b5c9024829a97ae4156e0231ac681d36675b814
datalexum <alexander@schroeder-de.com>
2022-11-21 20:55:59 +0100

    Add basic hash function

--- src/utils/hash.py
+++ src/utils/hash.py
@@ -1,8 +1,26 @@
+import functools
 from abc import ABC, abstractmethod
 
 
 class HashFunctionGenerator(ABC):
 
     @abstractmethod
-    def get_function(self, min_value=0, max_value=20):
+    def get_function(self, max_value=20, add_param=0):
         pass
+
+    @staticmethod
+    @abstractmethod
+    def _hash_function(max_value: int, add_param: int, word: str):
+        pass
+
+
+class BasicHashFunctionGenerator(HashFunctionGenerator, ABC):
+    def get_function(self, max_value=20, add_param=0):
+        return functools.partial(self._hash_function, max_value, add_param)
+
+    @staticmethod
+    def _hash_function(max_value: int, add_param: int, word: str):
+        sum_ordinals = add_param
+        for letter in word:
+            sum_ordinals += ord(letter)
+        return sum_ordinals % max_value
