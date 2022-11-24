3c4915ef8145f18422c7f327a34d67fc9d5ed8fd
datalexum <alexander@schroeder-de.com>
2022-11-24 21:26:17 +0100

    Finish dataloader, add query

--- src/database_loader/database_loader.py
+++ src/database_loader/database_loader.py
@@ -7,28 +7,34 @@ class DatabaseLoader:
     def __init__(self, dataBaseLinksList: list):
         self.dataBaseLinksList = dataBaseLinksList
         self.dataBase = rdflib.Graph()
-        for link in dataBaseLinksList:
-            self.dataBase.parse(link, format="ttl")
-    """
-        for linkidx in range (0, len(dataBaseLinksList),1):
-            print("hi")
-            if dataBaseLinksList[linkidx][0] == "":
-                self.dataBase.parse(dataBaseLinksList[linkidx])
-            elif link[0] == "ttl":
-                self.dataBase.parse(link, format="ttl")
-        for triple in self.dataBase:
-            pprint.pprint(triple)
-    """
+
+        for linkidx, link in enumerate(dataBaseLinksList):
+            #print(linkidx)
+            #print(link)
+            #print(dataBaseLinksList[linkidx][1])
+
+            if dataBaseLinksList[linkidx][1] == "":
+                self.dataBase.parse(dataBaseLinksList[linkidx][0])
+            elif dataBaseLinksList[linkidx][1] == "ttl":
+                self.dataBase.parse(dataBaseLinksList[linkidx][0], format="ttl")
+            else:
+                print("Unknown database type")
+        print("Database created")
+
 
     def add_Databases(self, dataBaseLinksList: list):
         print(dataBaseLinksList)
-    """
-        for link in dataBaseLinksList:
-            if link[0] == "":
-                self.dataBase.parse(link)
-            elif link[0] == "ttl":
-                self.dataBase.parse(link, format="ttl")
-    """
+
+        for linkidx, link in enumerate(dataBaseLinksList):
+            if dataBaseLinksList[linkidx][1] == "":
+                self.dataBase.parse(dataBaseLinksList[linkidx][0])
+            elif dataBaseLinksList[linkidx][1] == "ttl":
+                self.dataBase.parse(dataBaseLinksList[linkidx][0], format="ttl")
+            else:
+                print("Unknown database type")
+
+        print("Database changed")
+
 
     def return_Databases(self):
         return self.dataBase
--- src/debugMain.py
+++ src/debugMain.py
@@ -5,9 +5,26 @@ if __name__ == '__main__':
 
     datalist = [["https://raw.githubusercontent.com/maribelacosta/linkeddata/master/data/alan_turing.ttl","ttl"]]
     Loader = DatabaseLoader(datalist)
-    #Loader.add_Databases("../data/dummyDataFile.nt")
+    Loader.add_Databases([["../data/dummyDataFile.nt",""]])
 
-    data = Loader.return_Databases()
-    print(data)
-    for triple in data:
-        print(triple)
+    data_graph = Loader.return_Databases()
+    #print(data)
+    #for triple in data_graph:
+    #    print(triple)
+
+    join_query = """
+    PREFIX ac:  <https://raw.githubusercontent.com/maribelacosta/linkeddata/master/data/alan_turing.ttl#>
+    PREFIX w3: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX xmn: <http://xmlns.com/foaf/0.1/>  
+    SELECT ?x ?duplic ?y
+    WHERE 
+    {
+        ?x w3:type ?duplic .
+        ?y ac:discipline ?duplic .
+    }
+    """
+
+    results = data_graph.query(join_query)
+    print("Results are")
+    for result in results:
+        print(f"{result.x} {result.duplic} {result.y}")
