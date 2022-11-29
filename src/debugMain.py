55b94590596aef809ed45ae9d767a06450b896ee
Lukas Trost <lukas.trost@rub.de>
2022-11-29 19:19:33 +0100

    Added template queries

--- src/debugMain.py
+++ src/debugMain.py
@@ -1,5 +1,5 @@
 from database_loader.database_loader import DatabaseLoader
-
+from query_templates.queries import object_object_join,object_subject_join,bound_object_subject_join
 
 if __name__ == '__main__':
 
@@ -9,22 +9,55 @@ if __name__ == '__main__':
 
     data_graph = Loader.return_Databases()
     #print(data)
-    #for triple in data_graph:
-    #    print(triple)
+    for triple in data_graph:
+        print(triple)
 
-    join_query = """
-    PREFIX ac:  <https://raw.githubusercontent.com/maribelacosta/linkeddata/master/data/alan_turing.ttl#>
-    PREFIX w3: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX xmn: <http://xmlns.com/foaf/0.1/>  
-    SELECT ?x ?duplic ?y
-    WHERE 
-    {
-        ?x w3:type ?duplic .
-        ?y ac:discipline ?duplic .
-    }
-    """
+    #join_query = """
+    #PREFIX ac:  <https://raw.githubusercontent.com/maribelacosta/linkeddata/master/data/alan_turing.ttl#>
+    #PREFIX w3: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    #PREFIX xmn: <http://xmlns.com/foaf/0.1/>
+    #SELECT ?x ?duplic ?y
+    #WHERE
+    #{
+    #    ?x w3:type ?duplic .
+    #    ?y ac:discipline ?duplic .
+    #}
+    #"""
+    #print(join_query)
+    #w3 = "w3"
+    #test_query = """
+    #PREFIX ac:  <https://raw.githubusercontent.com/maribelacosta/linkeddata/master/data/alan_turing.ttl#>
+    #PREFIX w3: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    #SELECT ?x ?duplic ?y
+    #WHERE
+    #{
+    #    ?x """ + w3 + """:type ?duplic .
+    #    ?y ac:discipline ?duplic .
+    #}
+    #"""
+    #print(test_query)
 
-    results = data_graph.query(join_query)
-    print("Results are")
-    for result in results:
-        print(f"{result.x} {result.duplic} {result.y}")
+    #results = data_graph.query(test_query)
+    #print("Results are")
+
+    #for result in results:
+    #    print(f"{result.x} {result.duplic} {result.y}")
+    pred_prefix_1 = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+    pred_prefix_2 = "<https://raw.githubusercontent.com/maribelacosta/linkeddata/master/data/alan_turing.ttl#>"
+    obj_prefix_1 = "<https://raw.githubusercontent.com/maribelacosta/linkeddata/master/data/alan_turing.ttl#>"
+
+    o_o_results = object_object_join(data_graph = data_graph, predicate1 = "type", predicate2 = "discipline", pred_prefix_1 = pred_prefix_1, pred_prefix_2 = pred_prefix_2)
+
+    o_s_results = object_subject_join(data_graph = data_graph, predicate1 = "type", predicate2 = "discipline", pred_prefix_1 = pred_prefix_1, pred_prefix_2 = pred_prefix_2)
+
+    bound_o_s_results = bound_object_subject_join(data_graph = data_graph, predicate1 = "type", predicate2 = "discipline", object1 = "University_of_Cambridge", pred_prefix_1 = pred_prefix_1, pred_prefix_2 = pred_prefix_2,obj_prefix_1 = obj_prefix_1)
+
+    print("Results for obj obj")
+    for result in o_o_results:
+        print(f"{result.sub1} {result.obj} {result.sub2}")
+    print("Results for obj sub")
+    for result in o_s_results:
+        print(f"{result.sub} {result.obj}")
+    print("Results for bound obj sub")
+    for result in bound_o_s_results:
+        print(f"{result.sub}")
---
+++ src/query_templates/queries.py
@@ -0,0 +1,79 @@
+import rdflib
+import pprint
+
+
+def object_object_join(data_graph, predicate1, predicate2, pred_prefix_1, pred_prefix_2):
+
+    pre1 = """pre1:"""
+    pre2 = """pre2:"""
+    prefixes =  """PREFIX pre1: """ + pred_prefix_1
+    if pred_prefix_1 != pred_prefix_2:
+        prefixes = prefixes + """PREFIX pre2: """ + pred_prefix_2
+    else:
+        pre2 = pre1
+
+    join_query = prefixes + """
+    SELECT ?sub1 ?obj ?sub2
+    WHERE 
+    {
+        ?sub1 """ + pre1 + predicate1 + """ ?obj.
+        ?sub2 """ + pre2 + predicate2 + """ ?obj.
+    }
+    """
+    #print("obj obj")
+    #print(join_query)
+    return data_graph.query(join_query)
+
+
+def object_subject_join(data_graph, predicate1, predicate2, pred_prefix_1, pred_prefix_2):
+    pre1 = """pre1:"""
+    pre2 = """pre2:"""
+    prefixes = """PREFIX pre1: """ + pred_prefix_1
+    if pred_prefix_1 != pred_prefix_2:
+        prefixes = prefixes + """PREFIX pre2: """ + pred_prefix_2
+    else:
+        pre2 = pre1
+
+    join_query = prefixes + """
+    SELECT ?sub ?obj
+    WHERE 
+    {
+        ?sub """ + pre1 + predicate1 + """ ?obj.
+        ?sub """ + pre2 + predicate2 + """ ?obj.
+    }
+    """
+    #print("obj sub")
+    #print(join_query)
+    return data_graph.query(join_query)
+
+
+def bound_object_subject_join(data_graph, predicate1, predicate2, object1, pred_prefix_1, pred_prefix_2,obj_prefix_1):
+    pre1 = """pre1:"""
+    pre2 = """pre2:"""
+    pre3 = """pre3:"""
+    prefixes = """PREFIX pre1: """ + pred_prefix_1 + """ """
+
+    if pred_prefix_1 != pred_prefix_2:
+        prefixes = prefixes + """PREFIX pre2: """ + pred_prefix_2 + """ """
+    else:
+        pre2 = pre1
+
+    if obj_prefix_1 == pred_prefix_1:
+        pre3 = pre1
+    elif obj_prefix_1 == pred_prefix_2:
+        pre3 = pre2
+    else:
+        prefixes = prefixes + """PREFIX pre3: """ + obj_prefix_1 + """ """
+
+
+    join_query = prefixes + """
+    SELECT ?sub 
+    WHERE 
+    {
+        ?sub """ + pre1 + predicate1 + """ """ + pre3 + object1+""".
+        ?sub """ + pre2 + predicate2 + """ """ + pre3 + object1+""".
+    }
+    """
+    #print("bound obj sub")
+    #print(join_query)
+    return data_graph.query(join_query)
