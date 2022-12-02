8e7988d7aee825e37b2766ce265c8ba631d82bc5
datalexum <alexander@schroeder-de.com>
2022-12-02 17:04:58 +0100

    Changes from meeting

--- src/cardinality_debugMain.py
+++ src/cardinality_debugMain.py
@@ -13,25 +13,6 @@ if __name__ == '__main__':
     pred_prefix_1 = "<https://test.com/whatever#>"
     pred_prefix_2 = "<https://test.com/whatever#>"
 
-    # o_o_results = object_object_join(data_graph = data_graph, predicate1 = "predicate", predicate2 = "predicate2", pred_prefix_1 = pred_prefix_1, pred_prefix_2 = pred_prefix_2)
-
-    # print("Results for obj obj")
-    # for result in o_o_results:
-    #    print(f"{result.sub1} {result.obj} {result.sub2}")
-    # print(len(o_o_results))
-
-    o_o_cms_p1 = CMS(10, 2)
-    o_o_cms_p2 = CMS(10, 2)
-
-    o_o_cms_result = count_object_object(o_o_cms_p1, o_o_cms_p2, "<https://test.com/whatever#predicate>",
-                                     "<https://test.com/whatever#predicate2>", data_graph)
-
-    o_o_query_result = len(object_object_join(data_graph=data_graph, predicate1="predicate", predicate2="predicate2",
-                                      pred_prefix_1=pred_prefix_1, pred_prefix_2=pred_prefix_2))
-
-    print(f"CMS Result: {o_o_cms_result}, Query Result: {o_o_query_result}")
-
-
     o_s_cms_p1 = CMS(10, 2)
     o_s_cms_p2 = CMS(10, 2)
 
--- src/debugMain.py
+++ src/debugMain.py
@@ -3,7 +3,7 @@ from query_templates.queries import object_object_join,object_subject_join,bound
 
 if __name__ == '__main__':
 
-    datalist = [["../data/yago-1.0.0-turtle_abridged.ttl","ttl"]]#[["https://yago-knowledge.org/data/yago1/yago-1.0.0-turtle.7z","ttl"]]#[["https://raw.githubusercontent.com/maribelacosta/linkeddata/master/data/alan_turing.ttl","ttl"]]
+    datalist = [["../data/yago-1.0.0-turtle.ttl","ttl"]]#[["https://yago-knowledge.org/data/yago1/yago-1.0.0-turtle.7z","ttl"]]#[["https://raw.githubusercontent.com/maribelacosta/linkeddata/master/data/alan_turing.ttl","ttl"]]
     Loader = DatabaseLoader(datalist)
 
     Loader.add_Databases([["../data/dummyDataFile.nt",""]])
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     #o_s_results = object_subject_join(data_graph = data_graph, predicate1 = "type", predicate2 = "discipline", pred_prefix_1 = pred_prefix_1, pred_prefix_2 = pred_prefix_2)
 
     #bound_o_s_results = bound_object_subject_join(data_graph = data_graph, predicate1 = "type", predicate2 = "discipline", object1 = "University_of_Cambridge", pred_prefix_1 = pred_prefix_1, pred_prefix_2 = pred_prefix_2,obj_prefix_1 = obj_prefix_1)
-
+"""
     print("Results for obj obj")
     for result in o_o_results:
         print(f"{result.sub1} {result.obj} {result.sub2}")
