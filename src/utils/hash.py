f652d71e951d13fd266683eb7073c919aff130f9
datalexum <alexander@schroeder-de.com>
2022-12-28 18:49:26 +0100

    Add UniversalHashFunction

--- src/utils/hash.py
+++ src/utils/hash.py
@@ -1,6 +1,7 @@
 import functools
 import random
 from abc import ABC, abstractmethod
+from typing import List
 
 import mmh3
 from Crypto.Util import number
@@ -13,7 +14,7 @@ class HashFunctionGenerator(ABC):
         pass
 
 
-class BasicHashFunctionGenerator(HashFunctionGenerator, ABC):
+class BasicHashFunction(HashFunctionGenerator, ABC):
     def get_function(self, max_value=20, param=None):
         if param is None:
             param = random.randrange(0, 10000)
@@ -68,3 +69,21 @@ class MMH3(HashFunctionGenerator, ABC):
     @staticmethod
     def _hash_function(max_value: int, param: int, word: str):
         return mmh3.hash(word, param) % max_value
+
+
+class UniversalHashFunction(HashFunctionGenerator, ABC):
+    def get_function(self, max_value=20, m=None):
+        if m is None:
+            m = max_value
+        else:
+            if not number.isPrime(m):
+                raise Exception("m must be prime")
+
+        a = [random.randrange(0, m) for _ in range(m)]
+        b = random.randrange(0, m)
+        return functools.partial(self._hash_function, max_value, m, a, b)
+
+    @staticmethod
+    def _hash_function(max_value: int, m: int, a: List[int], b: int, word: str):
+        x = sum([ord(letter) for letter in word])
+        return ((sum([a[i] * ord(word[i]) for i in range(len(word))]) + b) % m) % max_value
